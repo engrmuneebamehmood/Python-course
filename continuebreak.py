f78cc6448423232bcb5d16040a2817558801d26d
MUNEEBA MEHMOOD <124117980+engrmuneebamehmood@users.noreply.github.com>
2023-02-13 08:25:21 +0500

    Add files via upload

---
+++ continuebreak.py
@@ -0,0 +1,28 @@
+#to continue loop
+'''
+i=1
+while i<=5:
+    if i==3:#yh print nh ho
+        i=i+1
+
+        continue
+    print(i)
+    i=i+1
+    '''
+
+'''
+s1="muneeba mehmood"
+for x in s1:
+    if x in 'aeiou':#yh alphabet print nai hongy
+        continue
+    print (x)
+    '''
+'''
+s1="muneeba mehmood"
+for x in s1:
+    if x in ' ':#yh alphabet print nai hongy
+        break #ju break sy oper condition aye ge woh agr true hoti gyi tu us sy agy loop nai chaly ga
+    print (x)
+
+'''
+    
---
+++ list.py
@@ -0,0 +1,8 @@
+#list is a data type
+#to store multiple items
+#list is represented by []
+
+fruit_names =['apple','cherry','mango','grapes']
+print(fruit_names)
+print(type(fruit_names))
+print(fruit_names[2])
